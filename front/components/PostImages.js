6c65e7e98f207d0c4573ce2420ccd6ce18a41547
TaehwanGo <gth1123@naver.com>
2021-07-18 03:17:30 +0900

    2-9. 이미지 구현하기

--- front/components/PostImages.js
+++ front/components/PostImages.js
@@ -1,8 +1,77 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import PropTypes from 'prop-types';
+import { PlusOutlined } from '@ant-design/icons';
 
 const PostImages = ({ images }) => {
-  return <div>PostImages 구현중...</div>;
+  const [showImagesZoom, setShowImagesZoom] = useState(false);
+  const onZoom = useCallback(() => {
+    setShowImagesZoom(true);
+  }, []);
+  console.log('images.length', images.length);
+  if (images.length === 1) {
+    return (
+      <>
+        <img
+          role="presentation"
+          src={images[0].src}
+          alt={images[0].src}
+          onClick={onZoom}
+        />
+      </>
+    );
+  }
+  if (images.length === 2) {
+    return (
+      <>
+        <img
+          role="presentation"
+          src={images[0].src}
+          style={{ width: '50%', display: 'inline-block', padding: 10 }}
+          alt={images[0].src}
+          onClick={onZoom}
+        />
+        <img
+          role="presentation"
+          src={images[1].src}
+          style={{
+            width: '50%',
+            display: 'inline-block',
+            padding: '10px 10px 10px 0',
+          }}
+          alt={images[1].src}
+          onClick={onZoom}
+        />
+      </>
+    );
+  }
+  return (
+    <>
+      <div>
+        <img
+          role="presentation"
+          src={images[0].src}
+          width="50%"
+          alt={images[0].src}
+          onClick={onZoom}
+        />
+        <div
+          role="presentation"
+          style={{
+            display: 'inline-block',
+            width: '50%',
+            textAlign: 'center',
+            verticalAlign: 'middle',
+          }}
+          onClick={onZoom}
+        >
+          <PlusOutlined />
+          <br />
+          {images.length - 1}
+          개의 사진 더보기
+        </div>
+      </div>
+    </>
+  );
 };
 
 PostImages.propTypes = {
